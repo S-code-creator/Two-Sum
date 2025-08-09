dd411aafe70bc6c9a6b19610052bb94c3d707848
Md. Sabbir Hossen <mdshabbirhossen062@gmail.com>
2025-08-09 23:24:50 +0600

    add leetcode problem Two-Sum

---
+++ Two-Sum.js
@@ -0,0 +1,15 @@
+var twoSum = function(nums , target){
+    
+    for(let i = 0; i < nums.length; i++ ){
+
+        for(let j = i + 1; j < nums.length; j++){
+            
+            if( nums[i] + nums[j] == target){
+                return [i,j];
+            }
+        }
+    }
+    return null;
+}
+
+console.log(twoSum([11,7,13,10,20] , 9));
